6ba3f9474a78e81afae82114fae2456880806444
stAcy0890 <stacysarfo@hotmail.com>
2019-03-16 22:19:31 +0000

    Initial Commit

---
+++ Database.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import sqlAlchemy
+
+#DATABASE
+project_dir = ""
+app.config['SQLACHEMY_DATABASE_URL'] = ''
+db = SQLAlchemy()
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    item_name = db.Column(db.String(250))
+    quantity_left = db.Column(db.Integer)
+    revenue = db.Column(db.Float)
+    price = db.Column(db.Float)
+
+class Transaction(db.Model):
+    id=db.Column(db.Integer, primary_key = True)
+    person_transacting=db.Column(db.String(250))
+    item_name= db.Column(db.String(250))
+    quantity_requested= db.Column(db.Integer)
+    time_of_transaction = db.Column(db.String(50))
+    completed= db.Column(db.Boolean)
+
+
+
+
